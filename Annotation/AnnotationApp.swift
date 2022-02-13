cd8e4dba0ec5dc789db943addca5703ef04dccf4
Vaida <91354917+Vaida12345@users.noreply.github.com>
2022-02-13 17:12:18 +0800

    Supports import / export from menu bar.

--- Annotation/AnnotationApp.swift
+++ Annotation/AnnotationApp.swift
@@ -10,9 +10,38 @@ import SwiftUI
 @main
 struct AnnotationApp: App {
     
+    @State var file: AnnotationDocument = AnnotationDocument()
+    @State var isShowingExportDialog = false
+    @State var isShowingImportDialog = false
+    
     var body: some Scene {
-        DocumentGroup(newDocument: AnnotationDocument()) { file in
-            DocumentView(document: file.$document)
+        DocumentGroup(newDocument: { AnnotationDocument() }) { file in
+            ContentView()
+                .onAppear {
+                    self.file = file.document
+                }
+        }
+        .commands {
+            CommandGroup(replacing: .importExport) {
+                Section {
+                    Button("Import") {
+                        isShowingImportDialog = true
+                    }
+                    .keyboardShortcut("i")
+                    .fileImporter(isPresented: $isShowingImportDialog, allowedContentTypes: [.annotationProject, .folder, .image], allowsMultipleSelection: true) { result in
+                        guard let urls = try? result.get() else { return }
+                        file.annotations.importForm(urls: urls)
+                    }
+                    
+                    Button("Export...") {
+                        isShowingExportDialog = true
+                    }
+                    .fileExporter(isPresented: $isShowingExportDialog, document: file, contentType: .folder, defaultFilename: "Annotation Export") { result in
+                        guard let url = try? result.get() else { return }
+                        FinderItem(at: url)?.setIcon(image: NSImage(imageLiteralResourceName: "Folder Icon"))
+                    }
+                }
+            }
         }
     }
 }
